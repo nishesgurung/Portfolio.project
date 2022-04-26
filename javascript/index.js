b4784bec891d064e76c41fa32bba51cfe0473029
Nishes Gurung <93386747+nishesgurung@users.noreply.github.com>
2022-04-26 21:50:18 +0545

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+$(window).scroll(()=>{
+    if($(window).scrollTop()>1000)
+    {
+      $('.nav1').addClass('cool')
+    }
+    else
+    {
+      $('.nav1').removeClass('cool')
+    }
+
+
+  }
+
+  )
+  
